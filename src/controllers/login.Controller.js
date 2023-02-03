adbc4cb32fc9512945c2582032a010ba042bc733
Pétala Leite <petalaleite@gmail.com>
2023-02-03 16:13:27 -0300

    corrigindo erro

--- src/controllers/login.Controller.js
+++ src/controllers/login.Controller.js
@@ -10,11 +10,11 @@ const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!isBodyValid(email, password)) {
-      return res.status(401).json({ message: 'Some required fields are missing' });
+      return res.status(400).json({ message: 'Some required fields are missing' });
     }
     const user = await userService.getEmailAndPassword(email, password);
     if (!user || user.password !== password) {
-      return res.status(401).json({ message: 'Invalid fields' });
+      return res.status(400).json({ message: 'Invalid fields' });
     }
     const jwtConfig = {
       expiresIn: '3d',
