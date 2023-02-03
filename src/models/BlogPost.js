9a257af3bb69b3cfa9149f49863a13a19390a9c3
Pétala Leite <petalaleite@gmail.com>
2023-02-03 16:00:31 -0300

    corrigindo blogpost?

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -1,6 +1,6 @@
 
 module.exports = (sequelize, DataTypes) => {
-  const BlogPosts = sequelize.define('BlogPost', {
+  const BlogPost = sequelize.define('BlogPost', {
     id: { type: DataTypes.INTEGER, primaryKey: true },
     title: DataTypes.STRING,
     content: DataTypes.STRING,
@@ -12,8 +12,8 @@ module.exports = (sequelize, DataTypes) => {
     timestamps: false,
     underscored: true,
   })
-  BlogPosts.associate = (models) => {
-    BlogPosts.belongsTo(models.User, { foreignKey: 'userId', as: 'user' })
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { foreignKey: 'userId', as: 'user' })
   }
-  return BlogPosts
+  return BlogPost
 }
